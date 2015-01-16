d4d79ba292117e365fe24bed27f2723e7e0e57aa
nathanfaucett <nathanfaucett@gmail.com>
2015-01-15 21:56:13 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var isNode;
+
+
+if (typeof(Node) !== "undefined") {
+    isNode = function isNode(obj) {
+        return obj instanceof Node;
+    };
+} else {
+    isNode = function isNode(obj) {
+        return (
+            typeof(obj) === "object" &&
+            typeof(obj.nodeType) === "number" &&
+            typeof(obj.nodeName) === "string"
+        );
+    };
+}
+
+
+module.exports = isNode;
